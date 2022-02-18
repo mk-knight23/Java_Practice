0189ad9ba4f168980b84a1df0627c1a0be653dca
KAZI MUSHARRAF <mk.knight970@gmail.com>
2022-02-18 09:01:09 +0530

    Update basic1.java

--- basic1.java
+++ basic1.java
@@ -4,8 +4,8 @@ public class Main
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		Scanner input = new Scanner(System.in);
-		int num1 = input.nextInt("enter num1");
-		int num2 = input.nextInt("enter num2");
+		int num1 = input.nextInt();
+		int num2 = input.nextInt();
 		int sum = num1+num2;
 		System.out.println("SUm is " + sum);
         System.out.println("こんにちは");
