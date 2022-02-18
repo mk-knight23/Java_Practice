e462d7416b130600a2c6305270a9bdd9825b2532
KAZI MUSHARRAF <mk.knight970@gmail.com>
2022-02-18 09:06:00 +0530

    New CHnages

--- basic1.java
+++ basic1.java
@@ -1,21 +1,31 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
 import java.util.Scanner;
 public class Main
 {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		System.out.println("Sum Of 2 Numbers");
 		Scanner input = new Scanner(System.in);
 		int num1 = input.nextInt();
 		int num2 = input.nextInt();
 		int sum = num1+num2;
-		System.out.println("SUm is " + sum);
-        System.out.println("こんにちは");
+		System.out.println("summ is " + sum);
+        System.out.println("Hello In Japenese Kon'nichiwa : こんにちは");
         
         // if Statrments 
         if(true){
-            System.out.println("HelloMk");
+            System.out.println("If True Print Hello In chinese Nǐ hǎo : 你好");
         }
         
-        // while statements 
+        // while statements
+        System.out.println("Numbers From 1 TO 6");
         int count=1;
         while(count!=6){
             System.out.println(count);
@@ -23,7 +33,8 @@ public class Main
         }
         
         // for statements
-        for(int count1=7;count1!=12;count1++){
+        System.out.println("Numbers From 7 TO 14");
+        for(int count1=7;count1!=14;count1++){
             System.out.println(count1);
         }
 		
